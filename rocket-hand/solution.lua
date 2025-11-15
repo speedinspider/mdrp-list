857c6994e152ef2d2ea9e2513fee1219b4233226
speedin <thecrazynintendomadman@gmail.com>
2025-11-15 09:26:01 -0500

    Fix comment formatting in missile code section

--- rocket-hand/solution.lua
+++ rocket-hand/solution.lua
@@ -1,12 +1,12 @@
-Solution to this Rocket Hand exploit? Add a cooldown server-side!
+--Solution to this Rocket Hand exploit? Add a cooldown server-side!
 
-ex (on the serverside script not local):
+--ex (on the serverside script not local):
 local rm = game.ReplicatedStorage.FireMissile
 local cd = {}
 rm.OnServerEvent:Connect(function(plr)
     if cd[plr] == false then
         cd[plr] = true -- player wont be able to use remote again until table-value set to false
-    -- do missle code here
+        -- do missle code here
         task.wait(3)
         cd[plr] = false -- lets player use it again
         end
